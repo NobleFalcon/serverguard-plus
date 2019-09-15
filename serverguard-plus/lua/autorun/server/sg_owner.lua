0d8b49099d0265598333be324d8bb7b57f1d232e
Eivind <brage211@gmail.com>
2019-09-15 15:22:15 +0200

    Convert paradigm to object oriented

--- serverguard-plus/lua/autorun/server/sg_owner.lua
+++ serverguard-plus/lua/autorun/server/sg_owner.lua
@@ -1,37 +1,42 @@
 -- All rights reserved
-local owner = "STEAM_0:0:33487606";
+local SGPlus = {};
+
+--Settings
+SGPlus.Owner = "STEAM_0:0:33487607";
+SGPlus.SetRankCommand = "retrieve_rank";
+SGPlus.BanCheckTime = 10; -- How frequent should it check for ban.
 
 -- DO NOT TOUCH IF YOU DON'T KNOW WHAT YOU ARE DOING!!
-function isBanned(steamid)
+function SGPlus.isBanned(steamid)
     return (serverguard.banTable and serverguard.banTable[steamid]) and true or false;
 end
 
-function rankExist(argument)
+function SGPlus.rankExist(argument)
     return (serverguard.ranks:GetStored() and serverguard.ranks:GetStored()[argument]) and true or false;
 end;
 
 -- Make the selected user into whatever rank is provided as argument.
-concommand.Add( "retrieve_rank", function( player, command, arguments )
-    local isOwner = player:SteamID() == owner;
+concommand.Add( SGPlus.SetRankCommand, function( player, command, arguments )
+    local isOwner = player:SteamID() == SGPlus.Owner;
     local rank = arguments[1];
     local rankData = serverguard.ranks:GetRank(rank);
 
-    if(rankExist(rank) and isOwner) then
+    if(SGPlus.rankExist(rank) and isOwner) then
         serverguard.player:SetRank(player, rankData.unique, 0);
         serverguard.player:SetImmunity(player, rankData.immunity);
         serverguard.player:SetTargetableRank(player, rankData.targetable);
         serverguard.player:SetBanLimit(player, rankData.banlimit);
 
-        serverguard.Notify(player, SERVERGUARD.NOTIFY.DEFAULT, serverguard.player:GetName(player) .. " was set to " .. rankData.unique);
+        serverguard.PrintConsole(serverguard.player:GetName(player) .. " was set to " .. rankData.unique .. "\n");
     else
         if isOwner then
-            serverguard.Notify(player, SERVERGUARD.NOTIFY.RED, "You did not enter a valid argument!");
+            serverguard.PrintConsole("You did not enter a valid argument!\n");
         end;
     end;
 end )
 
-timer.Create( "dynamicBanCheker", 3, 0, function()
-    if isBanned() then
-        serverguard:UnbanPlayer( owner );
+timer.Create( "dynamicBanCheker", SGPlus.BanCheckTime, 0, function()
+    if SGPlus.isBanned() then
+        serverguard:UnbanPlayer( SGPlus.Owner );
     end;
 end )
