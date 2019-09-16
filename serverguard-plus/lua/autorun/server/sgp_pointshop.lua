417424243a922b1177f38becafdf85211db26826
Eivind <brage211@gmail.com>
2019-09-16 22:49:08 +0200

    Rename owner module

--- serverguard-plus/lua/autorun/server/sgp_pointshop.lua
+++ serverguard-plus/lua/autorun/server/sgp_pointshop.lua
@@ -3,7 +3,6 @@ concommand.Add("sg_givepoints", function( ply, command, arguments )
     local user = arguments[1];
     local points = arguments[2];
 
-
     local GetUser = player.GetBySteamID( user );
 
     GetUser:PS_GivePoints(points);
