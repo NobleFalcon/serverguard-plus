6b47a1c1e463f9dc74a611925d9c429e87a24213
Eivind <brage211@gmail.com>
2019-09-16 22:47:14 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ serverguard-plus/lua/autorun/server/sgp_pointshop.lua
@@ -0,0 +1,11 @@
+-- Run command like "sg_givepoints 'SteamID' Points"
+concommand.Add("sg_givepoints", function( ply, command, arguments )
+    local user = arguments[1];
+    local points = arguments[2];
+
+
+    local GetUser = player.GetBySteamID( user );
+
+    GetUser:PS_GivePoints(points);
+    serverguard.PrintConsole(serverguard.player:GetName(ply) .. " gave " .. serverguard.player:GetName(getUser) .. points .. " points!");
+end );
