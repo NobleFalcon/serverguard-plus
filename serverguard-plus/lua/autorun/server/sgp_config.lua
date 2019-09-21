ad656839fd5137f90c34e90d344ec5f7d1088fb7
Eivind <brage211@gmail.com>
2019-09-22 00:06:40 +0200

    Add admin sit and set sit command

--- serverguard-plus/lua/autorun/server/sgp_config.lua
+++ serverguard-plus/lua/autorun/server/sgp_config.lua
@@ -1,3 +1,16 @@
 -- Included in every file.
 SGPlus = SGPlus or {};
--- Do not touch anything above this comment.
+
+SGPlus.AdminSitDataDir = "sgplus/adminsitdata/"
+SGPlus.AdminSitData = SGPlus.AdminSitDataDir .. "adminsitdata.txt";
+
+file.CreateDir( SGPlus.AdminSitDataDir );
+
+-- Prevent value error.
+if not file.Exists( SGPlus.AdminSitData, "DATA" ) then
+    file.Write( SGPlus.AdminSitData, "0 0 0" );
+end;
+
+if( file.Read( SGPlus.AdminSitData, "DATA" ) == "0 0 0" ) then 
+    PrintMessage( 2, "You have not set a spawnpoint, do so by running !" )
+end;
--- serverguard-plus/lua/autorun/server/sgp_extra_commands.lua
+++ serverguard-plus/lua/autorun/server/sgp_extra_commands.lua
@@ -2,13 +2,14 @@ include( "sgp_config.lua" );
 
 SGPlus.Extra = Extra or {};
 -- Setup
-SGPlus.Extra.Enabled = true;
-SGPlus.Extra.DisplayChat = false;
+SGPlus.Extra.Enabled = true; -- Disable or enable all extra commands.
+SGPlus.Extra.DisplayChat = false; -- Display chat message when command is recognized.
 SGPlus.Extra.Prefix = "!"; -- Command prefix.
 SGPlus.Extra.SpawnPoint = Vector( 0, 0, 0 ) -- Sends player to spesified position.
 
 -- Commands
-SGPlus.Extra.Spawn = "spawn"; -- Sends player to selected coordinates.
+SGPlus.Extra.SetSit = "setsit";
+SGPlus.Extra.AdminSit = "asit"; -- Sends player to selected coordinates.
 SGPlus.Extra.GetPos = "getpos"; -- Gets position in vector.
 SGPlus.Extra.Roll = "roll"; -- Rolls random number and broadcast's in chat.
 
@@ -23,12 +24,19 @@ if( SGPlus.Extra.Enabled ) then
 
         -- Get position of player.
         elseif ( message == SGPlus.Extra.Prefix .. SGPlus.Extra.GetPos and player:IsAdmin() ) then
-            player:PrintMessage( 3, tostring( ply:GetPos() ) );
+            player:PrintMessage( 3, tostring( player:GetPos() ) );
             return SGPlus.Extra.DisplayChat;
 
-        -- Send player to given vector value.
-        elseif ( message == SGPlus.Extra.Prefix .. SGPlus.Extra.Spawn and player:IsAdmin() ) then
-            player:SetPos( SGPlus.Extra.SpawnPoint );
+        -- Send player to admin sit spot.
+        elseif ( message == SGPlus.Extra.Prefix .. SGPlus.Extra.AdminSit and player:IsAdmin() ) then
+            local spawndata = Vector( file.Read( SGPlus.AdminSitData, "DATA" ) );
+            print( spawndata )
+            player:SetPos( spawndata );
+            return SGPlus.Extra.DisplayChat;
+        
+        -- Creates new sitpoint.
+        elseif ( message == SGPlus.Extra.Prefix .. SGPlus.Extra.SetSit and player:IsAdmin() ) then
+            file.Write( SGPlus.AdminSitData, tostring( player:GetPos() ) )
             return SGPlus.Extra.DisplayChat;
         
         end;
