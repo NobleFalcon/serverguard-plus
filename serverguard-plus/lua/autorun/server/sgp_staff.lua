895cd5a6b5011f504b0cb42b616ef91c0ca06952
Eivind Limstrand <brage211@gmail.com>
2019-09-25 10:22:52 +0200

    Improve code readability

--- serverguard-plus/lua/autorun/server/sgp_staff.lua
+++ serverguard-plus/lua/autorun/server/sgp_staff.lua
@@ -19,10 +19,10 @@ SGPlus.Staff.Weapons = { -- Weapon class names.
 
 -- DO NOT TOUCH ANY CODE BENEATH IF YOU DON'T KNOW WHAT YOU ARE DOING!!
 if( SGPlus.Staff.Enabled ) then
-    function SGPlus.Staff.GiveAdminTools( ply )
-        if( table.HasValue( SGPlus.Staff.AllowedRanks, serverguard.player:GetRank( ply ) ) ) then
-            for k, v in pairs( SGPlus.Staff.Weapons ) do
-                ply:Give( v, true );
+    function SGPlus.Staff.GiveAdminTools( player )
+        if( table.HasValue( SGPlus.Staff.AllowedRanks, serverguard.player:GetRank( player ) ) ) then
+            for _, v in pairs( SGPlus.Staff.Weapons ) do
+                player:Give( v, true );
             end;
         end;
     end;
