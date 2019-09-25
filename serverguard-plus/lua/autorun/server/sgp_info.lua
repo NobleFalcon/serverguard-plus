4cfeb060d013e418c738d437a52b2470097307a3
Eivind Limstrand <brage211@gmail.com>
2019-09-25 12:08:15 +0200

    Update version

--- serverguard-plus/lua/autorun/server/sgp_info.lua
+++ serverguard-plus/lua/autorun/server/sgp_info.lua
@@ -1,6 +1,6 @@
 include( "sgp_init.lua" );
 
-SGPlus.Version = "1.0.0";
+SGPlus.Version = "1.0.1";
 
 -- Display current version.
 SGPlus.PrintConsole( string.format( "You are running SGuard+ version %s", SGPlus.Version ) );
