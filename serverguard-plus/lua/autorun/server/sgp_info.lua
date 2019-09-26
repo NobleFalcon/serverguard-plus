f6d5fe1b961aa5cd303519ee118cbb1ee5bec256
Eivind Limstrand <brage211@gmail.com>
2019-09-26 10:01:37 +0200

    Improve console readability

--- serverguard-plus/lua/autorun/server/sgp_info.lua
+++ serverguard-plus/lua/autorun/server/sgp_info.lua
@@ -3,4 +3,4 @@ include( "sgp_init.lua" )
 SGPlus.Version = "1.0.1"
 
 -- Display current version.
-SGPlus.PrintConsole( Color( 50, 194, 219 ) , string.format( "Loaded SGuard+ version %s", SGPlus.Version ) )
+SGPlus.PrintConsole( SGPlus.WHITE , string.format( "Loaded SGuard+ version %s", SGPlus.Version ) )
--- serverguard-plus/lua/autorun/server/sgp_init.lua
+++ serverguard-plus/lua/autorun/server/sgp_init.lua
@@ -4,10 +4,12 @@ SGPlus = SGPlus or {}
 -- Display messages in console
 SGPlus.WHITE = Color( 255, 255, 255 )
 SGPlus.RED = Color( 255, 0, 0 )
+SGPlus.DARKRED = Color( 220, 0, 0 )
 SGPlus.GREEN = Color( 0, 255, 0 )
-SGPlus.BLUE = Color( 0, 0, 255 )
+SGPlus.BLUE = Color( 50, 194, 219 )
 
 SGPlus.PrintConsole = function( color, log )
     local time = os.date( "%H:%M" )
-    MsgC( color or SGPlus.WHITE, string.format( "[%s][SGuard+] %s\n", time, log ) )
+    MsgC( SGPlus.BLUE, string.format( "[%s][SGuard+] ", time ),
+     color or SGPlus.WHITE, log .. "\n" )
 end
