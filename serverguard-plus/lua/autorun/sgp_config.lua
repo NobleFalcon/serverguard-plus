9efdd8b17e4821b50f64344bba1e63c24318c562
Eivind Limstrand <brage211@gmail.com>
2019-10-03 15:05:03 +0200

    Reorganize guidance comments

--- serverguard-plus/lua/autorun/sgp_config.lua
+++ serverguard-plus/lua/autorun/sgp_config.lua
@@ -2,51 +2,29 @@ SGPlus = SGPlus or {}
 SGPlus.Owner = SGPlus.Owner or {}
 SGPlus.Extra = SGPlus.Extra or {}
 SGPlus.Staff = SGPlus.Staff or {}
+-- Please do not touch any of the code above this comment, any changes may break the addon.
 
---[[
-    Owner
-]]--
 
--- Setup
-
+-- Owner
 SGPlus.Owner.Enabled = true -- Enable or disable the SGPlus Owner module.
 SGPlus.Owner.User = "STEAM_0:0:33487606" -- Owner's SteamID.
-
--- Commands / Feature settings
 
 SGPlus.Owner.SetRankCommand = "sgp_set_rank" -- Set rank of owner without rank restrictions.
 SGPlus.Owner.BanCheckTime = 10 -- How frequent should it check for ban.
 
 
-
---[[
-    Extra commands
-]]--
-
--- Setup
-
+-- Extra commands
 SGPlus.Extra.Enabled = true -- Enable or disable the SGPlus Extra module.
 SGPlus.Extra.DisplayChat = false -- Display chat message when command is recognized.
 SGPlus.Extra.Prefix = "!" -- Command prefix.
-
--- Command settings
 
 SGPlus.Extra.SetSit = "setasit" -- Sets admin sit position.
 SGPlus.Extra.AdminSit = "asit" -- Sends player to selected coordinates.
 SGPlus.Extra.GetPos = "getpos" -- Gets position in vector.
 SGPlus.Extra.Roll = "roll" -- Rolls random number and broadcast's in chat.
 
-
-
---[[
-    Extra commands
-]]--
-
--- Setup
-
+-- Staff
 SGPlus.Staff.Enabled = true -- Enable or disable the SGPlus Staff module.
-
--- Settings.
 
 SGPlus.Staff.PermittedGroups = { -- Unique rank names.
     "founder",
